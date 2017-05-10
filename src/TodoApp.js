66b3dcfa7d34c2ea8ff72968ddfb19c1a5f1b023
Paulo Melo <paulo.melo@petrobras.com.br>
2017-05-10 14:26:52 -0300

    Todo app

---
+++ src/TodoApp.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+
+class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {items: [this.getInitialValue()], text: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
+  }
+
+  getInitialValue() {
+    return { id: 1, text: 'First task' };
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+export default TodoApp;
