dba8c3b4002a55557bca3f0f358b6f2b2be7a7c3
Paulo Melo <peimelo@gmail.com>
2019-11-06 12:00:07 -0300

    Updates

---
+++ precache-manifest.e6f91251b32092ed1edf66acb51c93fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8013cece219e16dc3838635784d762",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f81621f09a9941a46815",
+    "url": "/react-todo/static/css/main.c35cbe0e.chunk.css"
+  },
+  {
+    "revision": "f3fa73cadfbb4f7d05ca",
+    "url": "/react-todo/static/js/2.1ef8f5e6.chunk.js"
+  },
+  {
+    "revision": "f81621f09a9941a46815",
+    "url": "/react-todo/static/js/main.fc024f3b.chunk.js"
+  },
+  {
+    "revision": "a15761acbe19c2fcadc9",
+    "url": "/react-todo/static/js/runtime-main.247ea2e8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-todo/static/media/logo.25bf045c.svg"
+  }
+]);
